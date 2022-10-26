93f8649c4af410e4ca387d93be09fa29a1bb04de
Kamalani <kamalani@dev-island.org>
2022-10-25 18:41:00 -1000

    shopbar

--- src/components/Page.js
+++ src/components/Page.js
@@ -2,13 +2,16 @@
 import { Banner } from "./Banner";
 import { Navbar } from "./Navbar";
 import { useEffect, useState } from "react";
+import { useGetDataQuery } from "../app/services/api";
+import { Shopbar } from "./Shopbar";
 
 export const Page = () => {
-  const [view, setView] = useState("products");
+  const { data: data } = useGetDataQuery();
   return (
     <div>
       <Navbar />
       <Banner />
+      <Shopbar />
     </div>
   );
 };
---
+++ src/components/Shopbar.js
@@ -0,0 +1,24 @@
+export const Shopbar = () => {
+  return (
+    <div className="shopbar">
+      <div className="card1">
+        <p>HEADPHONES</p>
+        <button>
+          SHOP <span>&gt;</span>{" "}
+        </button>
+      </div>
+      <div className="card2">
+        <p>SPEAKERS</p>
+        <button>
+          SHOP <span>&gt;</span>
+        </button>
+      </div>
+      <div className="card3">
+        <p>EARPHONES</p>
+        <button>
+          SHOP <span>&gt;</span>{" "}
+        </button>
+      </div>
+    </div>
+  );
+};
