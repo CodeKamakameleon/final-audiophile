d02f746b8f24277e3934b52a526c5427d98cc869
Kamalani <kamalani@dev-island.org>
2022-10-26 10:59:50 -1000

    image issues

--- src/components/Shopbar.js
+++ src/components/Shopbar.js
@@ -1,21 +1,25 @@
 export const Shopbar = () => {
   return (
     <div className="shopbar">
-      <div className="card1">
-        <p>HEADPHONES</p>
-        <button>
+      <div className="card">
+        <img
+          src="./images/category-headphones/mobile/image-xx99-mark-one.jpg"
+          alt="img-xx99-mark-1"
+        />
+        <p className="card-header">HEADPHONES</p>
+        <button className="card-btn">
           SHOP <span>&gt;</span>{" "}
         </button>
       </div>
-      <div className="card2">
-        <p>SPEAKERS</p>
-        <button>
+      <div className="card">
+        <p className="card-header">SPEAKERS</p>
+        <button className="card-btn">
           SHOP <span>&gt;</span>
         </button>
       </div>
-      <div className="card3">
-        <p>EARPHONES</p>
-        <button>
+      <div className="card">
+        <p className="card-header">EARPHONES</p>
+        <button className="card-btn">
           SHOP <span>&gt;</span>{" "}
         </button>
       </div>
