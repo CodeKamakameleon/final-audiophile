531559bfd22e513d487e076bafaff3b002aab1f6
Kamalani <kamalani@dev-island.org>
2022-11-14 13:50:07 -1000

    modal

--- src/components/Page.js
+++ src/components/Page.js
@@ -8,10 +8,13 @@ import { Featured } from "./Featured";
 import { Closer } from "./Closer";
 import { Footer } from "./Footer";
 import { Modal } from "./Modal";
+import clsx from "clsx";
+import { useSelector } from "react-redux";
 
 export const Page = () => {
+  const isOpen = useSelector((state) => state.modal.isOpen);
   return (
-    <div>
+    <div className={clsx(`page ${isOpen ? "page-modal-open" : ""}`)}>
       <Navbar />
       <Modal />
       <Banner />
